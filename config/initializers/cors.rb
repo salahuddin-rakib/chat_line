b04e159d3dc9c8f8ff8877ad60a3965f96355d65
gazi_salahuddin <gsalahuddin@relisource.com>
2023-06-11 14:53:01 +0600

    Fix cors issue.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    # Allow only the valid IP/Domain here.
+    # origins '*'
+    origins 'http://localhost:5173'
 
     resource "*",
       headers: :any,
